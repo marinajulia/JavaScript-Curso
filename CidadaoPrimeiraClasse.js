485f4232dda282b8bde6b71f7370298fb3726183
Marina Julia <marinajulia707@gmail.com>
2021-08-09 11:12:55 -0300

    funções

---
+++ CidadaoPrimeiraClasse.js
@@ -0,0 +1,28 @@
+
+//Forma literal
+function fun1() { }
+
+//Armazenar em uma variável
+
+const fun2 = function () { }
+
+//Armazenaar em um array
+
+const array = [function (a, b) { return a + b }, fun1 + fun2]
+
+console.log(array[0](2, 3))
+
+//armazenar em um atributo de objeto
+
+// const obj = {}
+
+// obj.falar = function(){ return 'opa'}
+// console.log(obj.falar());
+
+// passar função como parâmetro
+
+function run(fun) {
+    fun()
+}
+
+run(function () { console.log("Executando..") })

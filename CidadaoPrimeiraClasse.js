555018d63c3c056ff6ce171bdcfa7288fa82b900
Marina Julia <marinajulia707@gmail.com>
2021-08-09 14:39:27 -0300

    arrow

--- CidadaoPrimeiraClasse.js
+++ CidadaoPrimeiraClasse.js
@@ -9,20 +9,28 @@ const fun2 = function () { }
 //Armazenaar em um array
 
 const array = [function (a, b) { return a + b }, fun1 + fun2]
-
 console.log(array[0](2, 3))
 
 //armazenar em um atributo de objeto
 
-// const obj = {}
+const obj = {}
 
-// obj.falar = function(){ return 'opa'}
-// console.log(obj.falar());
+obj.falar = function () { return 'opa' }
+console.log(obj.falar());
 
 // passar função como parâmetro
 
 function run(fun) {
     fun()
 }
-
 run(function () { console.log("Executando..") })
+
+//uma função pode retornar/conter uma função
+
+function soma(a, b) {
+    return function (c) {
+        console.log(a + b + c)
+    }
+}
+soma(2, 3)(4)
+
